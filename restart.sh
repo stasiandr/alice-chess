c48bc760b2772639e2fe3502f24fa94a20bb5b4b
stasiandr <stasiandr@gmail.com>
2021-01-19 04:17:57 +0300

    restart.sh

--- restart.sh
+++ restart.sh
@@ -8,4 +8,4 @@ docker run -dp 5000:5000 --name app app
 docker stop nginx
 docker rm nginx
 docker build -t nginx ./nginx
-docker run -dp 80:80 --name nginx -v /etc/letsencrypt/live/stas.dtransform.online:/var/www/cert   nginx
+docker run -d -p 80:80 -p 443:443 --name nginx nginx
