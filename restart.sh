6a03f57ecda9bca2a46ee94048412d2a9ade8778
stasiandr <stasiandr@gmail.com>
2021-01-19 04:52:36 +0300

    restart.sh

--- restart.sh
+++ restart.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+docker network create mynet
+
 docker stop app
 docker rm app
 docker build -t app ./app
-docker run -dp 5000:5000 --name app app
+docker run -dp 5000:5000 --name app --net mynet app
 
 docker stop nginx
 docker rm nginx
 docker build -t nginx ./nginx
-docker run -d -p 80:80 -p 443:443 --name nginx -v /etc/letsencrypt:/etc/letsencrypt   nginx
+docker run -d -p 80:80 -p 443:443 --name nginx --net mynet -v /etc/letsencrypt:/etc/letsencrypt nginx
