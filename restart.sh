1a1b18d51d9161ae7ef9424e5a539f3bdd598e40
stasiandr <stasiandr@gmail.com>
2021-01-19 03:01:39 +0300

    upd: endings

--- restart.sh
+++ restart.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 docker stop app
-
 docker build -t app ./app
-
 docker run -dp 5000:5000 --name app app
+
+docker stop nginx
+docker build -t nginx ./nginx
+docker run -dp 5000:5000 --name nginx nginx
