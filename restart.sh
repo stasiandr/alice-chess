7ab8bcd0432d1a4b6bc0062338d680cdbae84c19
stasiandr <stasiandr@gmail.com>
2021-01-19 03:05:14 +0300

    fix: no remove

--- restart.sh
+++ restart.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 docker stop app
+docker rm app
 docker build -t app ./app
 docker run -dp 5000:5000 --name app app
 
 docker stop nginx
+docker rm nginx
 docker build -t nginx ./nginx
-docker run -dp 5000:5000 --name nginx nginx
+docker run -dp 80:80 --name nginx nginx
